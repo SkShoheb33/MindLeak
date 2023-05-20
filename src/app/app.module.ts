034e6e64345fe104bb5529922bcd3759e39202e8
SkShoheb33 <shaikshoheb9k@gmail.com>
2023-05-20 17:27:29 +0530

    This my version 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SplashscreenComponent } from './components/splashscreen/splashscreen.component';
@@ -11,6 +11,32 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { RegisterCardComponent } from './components/register-card/register-card.component';
 import { RegisterTrackerComponent } from './components/register-tracker/register-tracker.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { PostComponent } from './components/post/post.component';
+import { InputPostComponent } from './components/input-post/input-post.component';
+import { NewPostComponent } from './components/new-post/new-post.component';
+import { MainComponent } from './components/main/main.component';
+import { Routes,RouterModule } from '@angular/router';
+
+
+const routes:Routes =[
+  {
+    component:LoginComponent,
+    path:''
+  },
+   {
+    component:RegisterComponent,
+    path:'register'
+  },
+   {
+    component:MainComponent,
+    path:'home'
+  },
+  {
+    component:ProfileComponent,
+    path:'profile'
+  }
+];
 
 @NgModule({
   declarations: [
@@ -22,11 +48,18 @@ import { RegisterTrackerComponent } from './components/register-tracker/register
     NavbarComponent,
     SettingsComponent,
     RegisterCardComponent,
-    RegisterTrackerComponent
+    RegisterTrackerComponent,
+    ProfileComponent,
+    PostComponent,
+    InputPostComponent,
+    NewPostComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/register-card/register-card.component.ts
+++ src/app/components/register-card/register-card.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit,Input} from '@angular/core';
 @Component({
   selector: 'app-register-card',
   templateUrl: './register-card.component.html',
   styleUrls: ['./register-card.component.css']
 })
-export class RegisterCardComponent {
+export class RegisterCardComponent implements OnInit{
+  @Input ('questions') questions:any;
+  currentNode :any|undefined;
+  pDisabled:Boolean = true;
+  nDisabled:Boolean = false;
+  alert = true;
+  success:Boolean = false;
+  i = 0;
+  bName:String;
+  constructor(){
+    this.bName = ''
+     this.nDisabled= false;
+  }
+  ngOnInit(): void {
+    this.currentNode = this.questions[this.i];
+     this.nDisabled= false;
 
+  }
+  prev(answer:String){
+    console.log(answer);
+    this.nDisabled = false;
+    this.pDisabled = false;
+    this.questions[this.i].answer = answer;
+    this.i-=1;
+    this.currentNode = this.questions[this.i];
+    if(this.i<=0){ 
+      this.pDisabled = true;
+    }
+  }
+  next(answer:String){
+    if(answer.length==0){
+      this.alert = false;
+      setTimeout(()=>this.alert=true,500);
+
+    }else{
+      console.log(answer);
+      this.nDisabled = false;
+      this.pDisabled = false;
+      this.i +=1;
+      this.currentNode = this.questions[this.i];
+      if(this.i == 7){
+        this.nDisabled = true;
+      }
+    }
+  }
+  submit(){
+    console.log(this.questions);
+    this.success = true;
+    setTimeout(()=>this.success=false,500);
+  }
 }
